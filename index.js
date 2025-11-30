b5103912a0cc8d4915c3306191c7f5817d198194
kj295 <31934125+kj295@users.noreply.github.com>
2025-11-30 09:27:04 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "semantic-ui-css/semantic.min.css";
+import "./theme.css"; 
+import "./App.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
