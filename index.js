c7b8751e27931f490dfd66e0e05159f50a9bcfbf
kj295 <31934125+kj295@users.noreply.github.com>
2025-11-30 09:36:32 +0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "semantic-ui-css/semantic.min.css";
-import "./theme.css"; 
-import "./App.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
