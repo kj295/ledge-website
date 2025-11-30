194c09a4a8899226989c27cf008975ba1202b291
kj295 <31934125+kj295@users.noreply.github.com>
2025-11-30 09:33:41 +0400

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>© {new Date().getFullYear()} Ledge LLC — All Rights Reserved</p>
+    </div>
+  );
+}
+
+export default Footer;
